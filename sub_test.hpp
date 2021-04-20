d5ff29d6ba10fbc242350331a16e439d7b9ca5a3
jeffwang372 <jeffwang372@gmail.com>
2021-04-19 23:55:03 -0700

    Completed test files for sub and pow.

--- sub_test.hpp
+++ sub_test.hpp
@@ -21,6 +21,24 @@
  EXPECT_EQ(test->stringify(), "(7.000000-5.000000)");
  }
 
+TEST(SubTest, SubZero) {
+ Op* op1 = new Op(7.0);
+ Op* op2 = new Op(0.0);
+ Sub* test = new Sub(op1, op2);
+
+ EXPECT_EQ(test->evaluate(), 7.0);
+ EXPECT_EQ(test->stringify(), "(7.000000-0.000000)");
+ }
+
+TEST(SubTest, SubNegative) {
+ Op* op1 = new Op(7.0);
+ Op* op2 = new Op(-5.0);
+ Sub* test = new Sub(op1, op2);
+
+ EXPECT_EQ(test->evaluate(),12.0);
+ EXPECT_EQ(test->stringify(), "(7.000000--5.000000)");
+ }
+
  TEST(SubTest, SubRand) {
  Op* op1 = new Op(100);
  Rand* op2 = new Rand();
@@ -35,7 +53,7 @@
  Sub* test = new Sub(op1, op2);
 
  EXPECT_EQ(test->evaluate(), 15);
- EXPECT_EQ(test->stringify(), "(30.000000-15.000000)");
+ EXPECT_EQ(test->stringify(), "(30.000000-(5.000000*3.000000))");
  }
 
 TEST(SubTest, SubDiv) {
@@ -44,7 +62,7 @@ TEST(SubTest, SubDiv) {
  Sub* test = new Sub(op1, op2);
 
  EXPECT_EQ(test->evaluate(), 28);
- EXPECT_EQ(test->stringify(), "(30.000000-2.000000)");
+ EXPECT_EQ(test->stringify(), "(30.000000-(6.000000/3.000000))");
  }
 
  TEST(SubTest, SubAdd) {
@@ -53,7 +71,7 @@ TEST(SubTest, SubDiv) {
  Sub* test = new Sub(op1, op2);
 
  EXPECT_EQ(test->evaluate(), 22);
- EXPECT_EQ(test->stringify(), "(30.000000-8.000000)");
+ EXPECT_EQ(test->stringify(), "(30.000000-(5.000000+3.000000))");
  }
 
  TEST(SubTest, SubPow) {
@@ -64,7 +82,18 @@ TEST(SubTest, SubDiv) {
  Sub* test = new Sub(op1, op2);
 
  EXPECT_EQ(test->evaluate(), 26);
- EXPECT_EQ(test->stringify(), "(30.000000-4.000000)");
+ EXPECT_EQ(test->stringify(), "(30.000000-(2.000000**2.000000))");
+ }
+
+TEST(SubTest, SubSub) {
+ Op* op1 = new Op(30);
+ Op* opSub1 = new Op(4);
+ Op* opSub2 = new Op(2);
+ Sub* op2 = new Sub(opSub1, opSub2);
+ Sub* test = new Sub(op1, op2);
+
+ EXPECT_EQ(test->evaluate(), 28);
+ EXPECT_EQ(test->stringify(), "(30.000000-(4.000000-2.000000))");
  }
 
 #endif
